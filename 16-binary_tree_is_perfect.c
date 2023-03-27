68f0a3da9bc9d221dd291ef039d31e65b4310cc9
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-03-27 14:51:19 -0500

    task 17

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,7 +18,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		left = binary_tree_is_perfect(tree->left);
 		right = binary_tree_is_perfect(tree->right);
-		if (left == 2 && right == 2)
+		if (left == 1 && right == 1)
 			return (1);
 	}
 	return (0);
