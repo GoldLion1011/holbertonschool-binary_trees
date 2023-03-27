e8d540cb969cfe6a52d2cdcadf29f359cb5bcd3f
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-03-27 15:38:17 -0500

    task 14 redo

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+
+	if (tree->left != NULL)
+		left = 1 + binary_tree_balance(tree->left);
+	if (tree->right != NULL)
+		right = 1 + binary_tree_balance(tree->right);
+
 	return (left - right);
 }
