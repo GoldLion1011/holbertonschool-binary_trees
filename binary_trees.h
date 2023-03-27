b71bf3905ea3121cf28f8a612d5adf8de0634e33
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-03-27 14:32:50 -0500

    14 but doesnt compile

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -47,6 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 
 
 #endif /* BINARY_TREES_H */
