a72c15501d1b5e56fbef495f0ac0f5f0d881b502
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-03-27 14:08:52 -0500

    task 13 spaces

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,16 +8,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL)
-        count += binary_tree_nodes(tree->left);
-    if (tree->right != NULL)
-        count += binary_tree_nodes(tree->right);
-    if (tree->left != NULL || tree->right != NULL)
-        count++;
-    return (count);
+	if (tree->left != NULL)
+		count += binary_tree_nodes(tree->left);
+	if (tree->right != NULL)
+		count += binary_tree_nodes(tree->right);
+	if (tree->left != NULL || tree->right != NULL)
+		count++;
+	return (count);
 }
