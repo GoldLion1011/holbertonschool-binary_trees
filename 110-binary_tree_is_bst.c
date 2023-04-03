8ef7aa104feca55e53aca207b63b3371e5021050
GoldLion1011 <sarsh.holbie@Sarahs-MacBook-Pro-2.local>
2023-04-03 16:46:54 -0500

    task 1

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -2,6 +2,25 @@
 #include <limits.h>
 
 /**
+ * isBSTUtil - checks if a binary tree is a valid Binary Search Tree
+ * @tree: pointer to the root node of the tree to check
+ * @min: minimum value
+ * @max: maximum value
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ * If tree is NULL, return 0
+ */
+
+int isBSTUtil(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	return (isBSTUtil(tree->left, min, tree->n - 1) &&
+		isBSTUtil(tree->right, tree->n + 1, max));
+}
+
+/**
  * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, and 0 otherwise
@@ -12,5 +31,5 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (is_bst(tree, INT_MIN, INT_MAX));
+	return (isBSTUtil(tree, INT_MIN, INT_MAX));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -23,7 +23,7 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
+typedef struct binary_tree_s bst_t;
 /*static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);*/
 void binary_tree_print(const binary_tree_t *tree);
@@ -48,6 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int isBSTUtil(const binary_tree_t *tree, int min, int max);
 int binary_tree_is_bst(const binary_tree_t *tree);
 
 
